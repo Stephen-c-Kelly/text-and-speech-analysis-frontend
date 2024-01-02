6332c9b89ec3e55ea4563796b8467e074ec8af68
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-01-02 15:19:47 -0500

    added to script.js

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const listEl = document.querySelector('.speech-list')
+const detailEl = document.querySelector('.speech-detail')
+
+async function getSpeech() {
+  try {
+  const response = await axios.get('http://localhost:3000/api/speech')
+  console.log(`data from axios ${response.data}`)
+  return response.data
+} catch (error) {
+  console.error('getSpeech failed to load:', error)
+  }
+}
+// should return an array of objects with _id, id, title, body
+
+const showSpeechList = async () => {
+  const data = await getSpeech()
+  data.forEach(speech =>{
+    const listItem = document.createElement('li')
+    listItem.innerHTML = `
+      <a href="/speech/${speech._id}"> <h3 class = "speech-detail">${speech.title}</h3> </a>
+      <p>Speaker: ${speech.speakerFirstName} ${speech.speakerLastName}</p>
+      <p>Date: ${speech.date}</p>
+    `;  
+    listEl.appendChild(listItem)
+  })
+    
+}
+const render = ( ) => {
+    showSpeechList()
+  }
+
+render()
+
+const showSpeech = async() =>{
+  
+
+}
+// Click Events
+detailEl.addEventListener('click', (event) =>{
+  const clickedElement = event.target
+  const href = clickedElement.getAttribute('href')
+  showSpeech(href)
+})
+
+
+console.log(getSpeech())
