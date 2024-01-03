737b0935192449d408500e9aa845c79078d586b4
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-01-03 17:30:00 -0500

    confirming  PUT functionm works

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const listEl = document.querySelector('.speech-list')
 const detailEl = document.querySelector('.speech-detail')
 const editBtnEl = document.querySelector('.edit-save')
+const speechTitleEl = document.querySelector(`h1`)
+const speechNameEl = document.querySelector('.name')
+const speechDateEl = document.querySelector('.date')
+const speechTextEl = document.querySelector('.text')
 
 // axios functions
 
@@ -27,6 +31,7 @@ const getSpeechById = async (id) => {
 
 // displaying content 
 const showSpeechList = async () => {
+if (window.location.pathname.endsWith('/html/')) { 
   const data = await getSpeech()
   data.forEach(speech =>{
     const listItem = document.createElement('li')
@@ -37,7 +42,7 @@ const showSpeechList = async () => {
     `;  
     listEl.appendChild(listItem)
   })
-    
+}    
 }
 const render = ( ) => {
     showSpeechList()
@@ -68,43 +73,69 @@ showSpeech()
 
 const toggleEdit = () => {
   if (window.location.pathname.includes('speech.html')) {
-    // cached element references for speech.html page
-    const speechTitleEl = document.querySelector(`h1`)
-    const speechNameEl = document.querySelector('.name')
-    const speechDateEl = document.querySelector('.date')
-    const speechTextEl = document.querySelector('.text')
     
+    // split name into first and last
+    const fullName = speechNameEl.innerText
+    const nameParts = fullName.split(" ");
+    const firstName = nameParts[0];
+    const lastName = nameParts[1];
+
+    let title = speechTitleEl.innerText
+    console.log(`title is `, title)
+    let date = speechDateEl.innerText
+    let text = speechTextEl.innerText
 
     // save/edit button toggle
     if (editBtnEl.innerHTML === 'Edit') {
       editBtnEl.innerHTML = 'Save'
-      let title = speechTitleEl.innerText
-      console.log(`title is `, title)
-      let name = speechNameEl.innerText
-      let date = speechDateEl.innerText
-      let text = speechTextEl.innerText
-
+    
       speechTitleEl.innerHTML = `<textarea id="editTitle">${title}</textarea>`
 
-      speechNameEl.innerHTML= `<textarea id="speechNameEl">${name}</textarea>`
+      speechNameEl.innerHTML= `<textarea id="speechNameEl">${firstName} ${lastName}</textarea>`
       speechDateEl.innerHTML= `<textarea id="speechDateEl">${date}</textarea>`
       speechTextEl.innerHTML= `<textarea id="speechTextEl">${text}</textarea>`
-    
-      
+
       
 
       // additional styling can go here
 
     } else {
-      editBtnEl.innerHTML = 'Edit'
-      //additional styling can go here
-      
+      putSpeech()
+
+
+
+     
+      //additional styling can go here    
       // call PUT to save 
 
     }
   }
 }
 
+const putSpeech = async () => {
+  const savedSpeechEl = document.querySelector(`.savedMsg`)
+  try {
+    let payload = {}
+    const speechId = new URLSearchParams(window.location.search).get('speechId');
 
-// Click Events
+    payload.title = document.querySelector('#editTitle').value
+    payload.firstName = document.querySelector('#speechNameEl').value.split(" ")[0];
+    payload.lastName = document.querySelector('#speechNameEl').value.split(" ")[1];
+    payload.date = document.querySelector('#speechDateEl').value;
+    payload.text = document.querySelector('#speechTextEl').value;
+    const response = await axios.put(`http://localhost:3000/api/speech/${speechId}`, payload)
+    showSpeech()
+    savedSpeechEl.innerHTML = `<p class="savedMsg" >Update Saved Successfully</p>`
+   
+  } catch (error) {
+    console.log(`we had an error`, error);
+  }
+}
+
+
+// Click Events - Home
+
+// Click Events -- Speech
+if (window.location.pathname.includes('speech.html')) {
 editBtnEl.addEventListener('click', toggleEdit)
+}
